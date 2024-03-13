11680a76140af4a82ce8b807e3ff206af556f074
Tamires Caires <tamiresdec@gmail.com>
2024-03-13 12:45:25 -0300

    feat(login): implement login layout e style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import LoginLayout from './components/PageLayout/LoginLayout';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
-      <div className='text-6xl'>
-        Maintenance Control
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path='/login'
+            element={
+              <LoginLayout>
+                <Login />
+              </LoginLayout>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
-};
+}
 
 export default App;
